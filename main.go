f3cfe9e03011ab9b46138ff9c4d24f45492ac60e
arash.gh <gholamdokht.arash@gmail.com>
2025-02-16 21:35:37 +0330

    fix dupl metric definition

--- main.go
+++ main.go
@@ -1,48 +1,34 @@
 package main
 
 import (
-	"log"
-	"net/http"
+    "log"
+    "net/http"
 
-	"github.com/gin-gonic/gin"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/Arash-81/go-app/src/albums"
+    "github.com/gin-gonic/gin"
+    "github.com/prometheus/client_golang/prometheus/promhttp"
+    "github.com/Arash-81/go-app/src/albums"
+    "github.com/Arash-81/go-app/src/metrics"
 )
-
-var (
-	httpStatusCodes = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "http_requests_total",
-			Help: "Total number of HTTP requests by status code",
-		},
-		[]string{"code"},
-	)
-)
-
-func init() {
-	prometheus.MustRegister(httpStatusCodes)
-}
 
 func main() {
-	// initialise Gin router:
-	router := gin.Default()
+    // initialise Gin router:
+    router := gin.Default()
 
-	// Middleware to track status codes
-	router.Use(func(c *gin.Context) {
-		c.Next()
-		httpStatusCodes.WithLabelValues(http.StatusText(c.Writer.Status())).Inc()
-	})
+    // Middleware to track status codes
+    router.Use(func(c *gin.Context) {
+        c.Next()
+        metrics.RequestCounter.WithLabelValues(c.Request.Method, c.FullPath()).Inc()
+    })
 
-	router.GET("/albums", albums.GetAlbums)
-	router.POST("/albums", albums.PostAlbums)
+    router.GET("/albums", albums.GetAlbums)
+    router.POST("/albums", albums.PostAlbums)
 
-	// Prometheus metrics endpoint
-	router.GET("/metrics", gin.WrapH(promhttp.Handler()))
+    // Prometheus metrics endpoint
+    router.GET("/metrics", gin.WrapH(promhttp.Handler()))
 
-	// run the server on port 3000:
-	err := router.Run(":3000")
-	if err != nil {
-		log.Fatalf("[Error] failed to start Gin server due to: %v", err)
-	}
+    // run the server on port 3000:
+    err := router.Run(":3000")
+    if err != nil {
+        log.Fatalf("[Error] failed to start Gin server due to: %v", err)
+    }
 }
